7e373fd1fa12121ed70578aef87ebc585d4e96e6
Yuto Nakano <yuto.44.bb@gmail.com>
2018-12-28 07:37:15 +0900

    Initial Commit

---
+++ PracticeToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeToDo
+//
+//  Created by 中野湧仁 on 2018/12/28.
+//  Copyright © 2018年 中野湧仁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
